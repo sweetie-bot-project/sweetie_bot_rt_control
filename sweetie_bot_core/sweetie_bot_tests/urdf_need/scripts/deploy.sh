57678f013224fad0ae4f97a36054d951f5193e57
mutronics <fsdfur@gmail.com>
2016-08-20 00:30:17 +0300

    urdf_need deleted

--- sweetie_bot_core/sweetie_bot_tests/urdf_need/scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-PACKAGE_NAME=$(basename $( cd $(dirname $0) ; cd .. ; pwd -P ) )
-PACKAGE_PATH=$(rospack find $PACKAGE_NAME)
-deployer-gnulinux -s "$PACKAGE_PATH/config/deploy.ops" -linfo
