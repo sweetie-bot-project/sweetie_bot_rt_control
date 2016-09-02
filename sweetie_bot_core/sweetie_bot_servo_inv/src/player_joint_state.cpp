561882554120847560049aebdd2b00205137f7bd
goncharov_oi <goncharovoi@yandex.ru>
2016-09-02 15:11:22 +0300

    Sync port support.i Fixed error handling.

--- sweetie_bot_core/sweetie_bot_servo_inv/src/player_joint_state.cpp
+++ sweetie_bot_core/sweetie_bot_servo_inv/src/player_joint_state.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <sstream>
+#include <system_error>
 
 #include <rtt/Component.hpp>
 
@@ -8,13 +9,15 @@
 using namespace RTT;
 
 PlayerJointState::PlayerJointState(std::string const& name) : 
-	TaskContext(name)
+	TaskContext(name, PreOperational)
 {
-	this->addPort(joints_port)
+	this->addPort("joints", joints_port)
 		.doc("Trajectory read from file.");
+	this->addEventPort("sync", sync_port)
+		.doc("Timer sync port.");
 
 	this->addProperty("file_name", file_name)
-		.doc("Trajectory file name. Trajectory is stored one sample per line in format: pos1 vel1 [effort1] pos2 vel2 [effort2] ... Lines beginning from # are skipped.");
+		.doc("Trajectory file name. Trajectory is stored one sample per line in format: pos1 vel1 [effort1] pos2 vel2 [effort2] ... Lines beginning with '#' are skipped.");
 	this->addProperty("joint_names", joint_names)
 		.doc("Name of joints.");
 	this->addProperty("cycle", cycle)
@@ -28,80 +31,99 @@ PlayerJointState::PlayerJointState(std::string const& name) :
 		.set(false);
 }
 
-bool PlayerJointState::startHook()
+bool PlayerJointState::configureHook()
 {
 	Logger::In in("PlayerJointState");
 
-	std::ifstream file;
-	file.exceptions(std::istream::failbit | std::istream::badbit);
+	sample_dim = joint_names.size(); 
 
-	try {
-		file.open(file_name.c_str());
+	std::ifstream file(file_name);
+	std::istringstream ss;
+	std::string line, skip;
+	sample_index = 0;
+	while (getline(file, line)) {
+		double value;
+			
+		ss.str(line);
+		ss.seekg(0);
+		sample_index++;
 
-		std::string line;
-		std::istringstream ss;
-		ss.exceptions(std::istream::failbit | std::istream::badbit | std::istream::eofbit);
+		if (ss.peek() == '#') continue;		
 
-		sample_index = 0;
-		while (getline(file, line)) {
-			float value;
-				
-			ss.str(line);
-			sample_index++;
+		for(unsigned int i = 0; i < sample_dim; i++) {
+			if (skip_first_column) ss >> skip; 
+			ss >> value; 
+			trajectory_pos.push_back(value);
+			ss >> value;
+			trajectory_vel.push_back(value);
 
-			for(unsigned int i = 0; i < joint_names.size(); i++) {
-				ss >> value; 
-				trajectory_pos.push_back(value);
+			if (effort_presents) {
 				ss >> value;
-				trajectory_vel.push_back(value);
-
-				if (effort_presents) {
-					ss >> value;
-					trajectory_effort.push_back(value);
-				}
+				trajectory_effort.push_back(value);
 			}
 		}
-	} 
-	catch (std::ios_base::failure e) {
-		log(Error) << "Near line " << sample_index << " error: " << e.what() << endlog();
+
+		if (ss.fail())  {
+			log(Error) << "Unable parse line " << sample_index << "." << endlog();
+			return false;
+		}
+	}
+	if (file.fail() && !file.eof()) {
+		log(Error) << "Error reading line " << sample_index << " at " << file_name << ": " << std::error_code(errno, std::system_category()).message() << endlog();
 		return false;
 	}
 
-	sample_index = 0;
-	sample_dim = joint_names.size();
-
-	joints.name = joint_names;
+	joints.name.resize(sample_dim);
 	joints.position.resize(sample_dim);
 	joints.velocity.resize(sample_dim);
 	if (effort_presents) joints.effort.resize(sample_dim);
 	else joints.effort.resize(0);
-
 	joints_port.setDataSample(joints);
+
+	joints.name = joint_names;
+
+	log(Info) << "PlayerJointState configured!" << endlog();
+	return true;
+}
+
+
+bool PlayerJointState::startHook()
+{
+	Logger::In in("PlayerJointState");
+	
+	sample_index = 0;
+
+	RTT::os::Timer::TimerId timer_id;
+	sync_port.readNewest(timer_id);
 
 	log(Info) << "PlayerJointState started!" << endlog();
 	return true;
 }
 
+
 void PlayerJointState::updateHook()
 {
-	if (sample_index*sample_dim >= trajectory_pos.size()) {
-		if (cycle) sample_index = 0;
-		else this->stop();
+	RTT::os::Timer::TimerId timer_id;
+
+	if (this->getPeriod() != 0.0 || sync_port.read(timer_id) == NewData) {
+		if ((sample_index + 1)*sample_dim >= trajectory_pos.size()) {
+			if (cycle) sample_index = 0;
+			else this->stop();
+		}
+
+		int begin = sample_dim * sample_index;
+		int end = begin + sample_dim;
+
+		std::copy(&trajectory_pos[begin], &trajectory_pos[end], joints.position.begin());
+		std::copy(&trajectory_vel[begin], &trajectory_vel[end], joints.velocity.begin());
+		if (effort_presents) {
+			std::copy(&trajectory_effort[begin], &trajectory_effort[end], joints.effort.begin());
+		}
+
+		joints_port.write(joints);
+		sample_index++;
 	}
-
-	int begin = sample_dim * sample_index;
-	int end = begin + sample_dim;
-
-	std::copy(&trajectory_pos[begin], &trajectory_pos[end], joints.position.begin());
-	std::copy(&trajectory_vel[begin], &trajectory_vel[end], joints.velocity.begin());
-	if (effort_presents) {
-		std::copy(&trajectory_effort[begin], &trajectory_effort[end], joints.effort.begin());
-	}
-
-	joints_port.write(joints);
-	sample_index++;
 }
-
 
 
 void PlayerJointState::stopHook() {
--- sweetie_bot_core/sweetie_bot_servo_inv/src/player_joint_state.hpp
+++ sweetie_bot_core/sweetie_bot_servo_inv/src/player_joint_state.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <rtt/RTT.hpp>
+#include <rtt/os/Timer.hpp>
 
 #include <orocos/sensor_msgs/typekit/JointState.h>
 #include <orocos/sweetie_bot_hardware_herkulex_msgs/typekit/ServoGoal.h>
@@ -22,6 +23,7 @@ class PlayerJointState : public RTT::TaskContext
 	protected:
 		// PORTS
 		RTT::OutputPort<sensor_msgs::JointState> joints_port;
+		RTT::InputPort<RTT::os::Timer::TimerId> sync_port;
 
 		// PROPERTIES
 		std::string file_name;
@@ -32,6 +34,7 @@ class PlayerJointState : public RTT::TaskContext
 		
 	public:
 		PlayerJointState(std::string const& name);
+		bool configureHook();
 		bool startHook();
 		void updateHook();
 		void stopHook();
