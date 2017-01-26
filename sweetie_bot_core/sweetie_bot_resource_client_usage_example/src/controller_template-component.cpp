903cc6b4a3f65c69cab9e0671dfcbfe0cdd4c115
Oleg Goncharov <goncharovoi@yandex.ru>
2017-01-27 01:47:31 +0300

    ControllerTemplate refactoring.

--- sweetie_bot_core/sweetie_bot_resource_client_usage_example/src/controller_template-component.cpp
+++ sweetie_bot_core/sweetie_bot_resource_client_usage_example/src/controller_template-component.cpp
@@ -1,136 +1,130 @@
-#include "sweetie_bot_resource_client_usage_example-component.hpp"
+#include "controller_template-component.hpp"
 
 #include <rtt/Component.hpp>
-#include <rtt/Logger.hpp>
 
-#include <sweetie_bot_resource_control/sweetie_bot_resource_control_service.hpp>
+using namespace RTT;
 
-#include <iostream>
+namespace sweetie_bot {
+namespace motion {
+namespace controller {
 
-using RTT::Logger;
-
-ResourceClientExample::ResourceClientExample(std::string const& name) : TaskContext(name),
-  isActive(false)
+ControllerTemplate::ControllerTemplate(std::string const& name)  : 
+	TaskContext(name, RTT::base::TaskCore::PreOperational),
+	log("sweetie.motion.controller.template")
+	//log("sweetie.motion.controller." + name)
 {
-  initResources();
-
-  resource_client = getProvider<ResourceClient>("resource_client");
-
-  Logger::log(Logger::Info) << "ResourceClientExample constructed !" << Logger::endl;
-}
-
-void ResourceClientExample::initResources()
-{
-  // TODO: move to the parameter server
-  std::string tmp[] = {"leg1", "leg2", "leg3", "leg4"};
-  std::vector<std::string> resources(tmp, tmp+sizeof(tmp) / sizeof(tmp[0]));
-  for (std::vector<std::string>::iterator name = resources.begin(); name != resources.end(); name++)
-  {
-	 this->resourcesRequiredPrimary[*name] = 1.0;
-	 Logger::log(Logger::Info) << "Primary resource initialized: " << *name << " with priority "
-	 	<< this->resourcesRequiredPrimary[*name] << Logger::endl;
-  }
-
-  resources.clear();
-  resources.push_back(std::string("head"));
-  for (std::vector<std::string>::iterator name = resources.begin(); name != resources.end(); name++)
-  {
-	 this->resourcesRequiredAuxiliary[*name] = 1.0;
-	 Logger::log(Logger::Info) << "Auxiliary resource initialized: " << *name << " with priority "
-	 	<< this->resourcesRequiredAuxiliary[*name] << Logger::endl;
-  }
-}
-
-/* Tries to start the controller.
- *
- * Sends a resource request, a reply to which will be processed by the plugin's
- * callback and the controller will be activated and start doing its task
- * in the updateHook if all resources were allocated or not, if some are lacking.
- * This is checked using the controller's resourceChangedHook.
- */
-bool ResourceClientExample::activate()
-{
-  resource_client->requestResources(resourcesRequiredPrimary);
+	resource_client = getProvider<ResourceClient>("resource_client").get();
+	// ports
+	// properties
+	std::vector<std::string> res = { "leg1", "leg2", "leg3", "leg4" };
+	this->addProperty("required_resources", resources_required).
+		doc("List of required resources.").
+		set(res);
+	// operations
+	this->addOperation("resourceChangedHook", &ControllerTemplate::resourceChangedHook, this).
+		doc("Check if all necessary resources present and component ready to be set operational.");
+	// services
+	if (!resource_client) {
+		log(ERROR) << getName() << "Unable to load `resource_client` service!" << endlog();
+		this->exception();
+	}
+	// other actions
+	log(INFO) << "ControllerTemplate is constructed!" << endlog();
 }
 
 /*	Determines if the controller can function normally in the given situation or not. 
  *	Usually it just checks if all the required resources have been allocated to it.
  *	The isOperational flag will be set by the plugin.
  */
-bool ResourceClientExample::resourceChangedHook()
+bool ControllerTemplate::resourceChangedHook()
 {
-  // TODO
-  for (std::map<std::string, double>::iterator it = resourcesRequiredPrimary.begin();
-  		it != resourcesRequiredPrimary.end(); ++it)
-  {
-	 // if a resource is not owned then activation is not possible
-	 if (!resource_client->hasResource(it->first))
-	 {
+	log(INFO) << "ControllerTemplate resourceChangedHook is being run!" << endlog();
+	// unable to run if dont have a leg.
+	if (resource_client->hasResource("leg1")) log(INFO) << "ControllerTemplate have a leg." << endlog();
+	else log(INFO) << "ControllerTemplate do not have a leg." << endlog();
+	// check if all resources present (if resourceChangedHook is not declared, resources client check this condition automatically).
+	if (!resource_client->hasResources(resources_required)) {
+		log(INFO) << "ControllerTemplate do NOT HAVE all resources." << endlog();
 		return false;
-	 }
-  }
-
-  return true;
+	}
+	log(INFO) << "ControllerTemplate HAVE all resources." << endlog();
+	// perform opertional condition checks and state reset if necessary
+	return true;
 }
 
-bool ResourceClientExample::configureHook()
+bool ControllerTemplate::configureHook()
 {
-  Logger::log(Logger::Info) << "ResourceClientExample configured !" << Logger::endl;
-
-  return true;
+	// INITIALIZATION
+	// get properties
+	// check if necessary ports and operations are connected
+	// allocate memory
+	// set ports data samples
+	log(INFO) << "ControllerTemplate is configured !" << endlog();
+	return true;
 }
 
-bool ResourceClientExample::startHook()
-{
-  Logger::log(Logger::Info) << "ResourceClientExample started !" << Logger::endl;
-
-  return true;
-}
-
-void ResourceClientExample::updateHook()
-{
-  Logger::log(Logger::Info) << "ResourceClientExample executes updateHook !" << Logger::endl;
-
-  // main logic
-  
-  if (resource_client->isOperational())
-  {
-	 doUsefulStuff();
-  } 
-}
-
-void ResourceClientExample::doUsefulStuff()
-{
-  // TODO: calculate and send speeds of the end of legs
-  Logger::log(Logger::Info) << "Calculating movement" << Logger::endl;
-
-}
-
-/* Preempts the controllers and releases its resources.
+/* 
+ * Tries to make the controller operational. 
  *
+ * Sends a resource request, a reply to which will be processed by the plugin's
+ * callback and the controller will be activated and start doing its task
+ * in the updateHook if all resources were allocated or not, if some are lacking.
+ * This is checked using the controller's resourceChangedHook.
  */
-void ResourceClientExample::stopHook() 
+bool ControllerTemplate::startHook()
 {
-  Logger::log(Logger::Info) << "ResourceClientExample executes stopping !" <<Logger::endl;
+	// reset state variables
+	// check if controller can be set operational in current conditions
+	// request resources
+	resource_client->requestResources(resources_required);
 
-  resource_client->stopOperational();
+	// now update hook will be periodically executed
+	log(INFO) << "ControllerTemplate is started !" << endlog();
+	return true;
 }
 
-void ResourceClientExample::cleanupHook() 
+void ControllerTemplate::updateHook()
 {
-  Logger::log(Logger::Info) << "ResourceClientExample cleaning up !" <<Logger::endl;
+	// let resource_client do it stuff
+	resource_client->step();	
+
+	// main operational 
+	if (resource_client->isOperational()) {
+		log(DEBUG) << "ControllerTemplate executes updateHook!" << endlog();
+	}
 }
+
+/* 
+ * Preempts the controllers and releases its resources.
+ */
+void ControllerTemplate::stopHook() 
+{
+	// deinitialization
+	// release all resources
+	resource_client->stopOperational();
+	log(INFO) << "ControllerTemplate is stopped!" << endlog();
+}
+
+void ControllerTemplate::cleanupHook() 
+{
+	// free memory, close files and etc
+	log(INFO) << "ControllerTemplate cleaning up !" << endlog();
+}
+
+} // namespace controller
+} // namespace motion
+} // namespace sweetie_bot
 
 /*
  * Using this macro, only one component may live
  * in one library *and* you may *not* link this library
  * with another component library. Use
  * ORO_CREATE_COMPONENT_TYPE()
- * ORO_LIST_COMPONENT_TYPE(ResourceClientExample)
+ * ORO_LIST_COMPONENT_TYPE(ControllerTemplate)
  * In case you want to link with another library that
  * already contains components.
  *
  * If you have put your component class
  * in a namespace, don't forget to add it here too:
  */
-ORO_CREATE_COMPONENT(ResourceClientExample)
+ORO_CREATE_COMPONENT(sweetie_bot::motion::controller::ControllerTemplate)
--- sweetie_bot_core/sweetie_bot_resource_client_usage_example/src/controller_template-component.hpp
+++ sweetie_bot_core/sweetie_bot_resource_client_usage_example/src/controller_template-component.hpp
@@ -1,42 +1,62 @@
 #ifndef OROCOS_SWEETIE_BOT_RESOURCE_CLIENT_USAGE_EXAMPLE_COMPONENT_HPP
 #define OROCOS_SWEETIE_BOT_RESOURCE_CLIENT_USAGE_EXAMPLE_COMPONENT_HPP
 
-#include <rtt/RTT.hpp>
-
-#include <sweetie_bot_resource_control/sweetie_bot_resource_control_service.hpp>
-
+#include <vector>
 #include <string>
 
-class ResourceClientExample : public RTT::TaskContext
+#include <rtt/Component.hpp>
+
+#include <sweetie_bot_logger/logger.hpp>
+#include <sweetie_bot_resource_control/resource_client-service.hpp>
+
+namespace sweetie_bot {
+
+namespace motion {
+
+namespace controller {
+
+
+class ControllerTemplate : public RTT::TaskContext
 {
-  protected:
-	 // maps a resource that is absolutely essential to the component
-	 // to a priority in range [0:1] that indicates how important this
-	 // resource is to this user among all possible other users
-	 std::map<std::string, double> resourcesRequiredPrimary;
+	protected:
+		// COMPONENT INTERFACE
+		//
+		// PORTS: input
+		// PORTS: output
+		// PROPERTIES
+		std::vector<std::string> resources_required;
+	protected:
+		// OPERATIONS: provides
+		// OPERATIONS: requires
+		// SERVICES:
+		sweetie_bot::motion::ResourceClient * resource_client;
 
-	 // the same as resourcesRequiredPrimary, but the resources here
-	 // are secondary to the component and it can function without them
-	 // (but it would still like to get ahold of them)
-	 std::map<std::string, double> resourcesRequiredAuxiliary;
+	protected:
+		// COMPONENT STATE
+		// ports buffers
+		
+#ifdef SWEETIEBOT_LOGGER
+		SWEETIEBOT_LOGGER log;
+#else
+		sweetie_bot::LoggerRTT log;
+#endif
 
-	 boost::shared_ptr<ResourceClient> resource_client;
-	 
-	 void initResources();
-	 void doUsefulStuff();
+	public:
+		ControllerTemplate(std::string const& name);
 
-	 bool isActive;
+		bool resourceChangedHook();
 
-  public:
-    ResourceClientExample(std::string const& name);
-    bool configureHook();
-    bool startHook();
-    void updateHook();
-    void stopHook();
-    void cleanupHook();
-
-    bool activate();
-
-    bool resourceChangedHook();
+		bool configureHook(); 
+		bool startHook();
+		void updateHook();
+		void stopHook();
+		void cleanupHook();
 };
+
+} // namespace controller
+
+} // namespace motion
+
+} // namespace sweetie_bot
+
 #endif
