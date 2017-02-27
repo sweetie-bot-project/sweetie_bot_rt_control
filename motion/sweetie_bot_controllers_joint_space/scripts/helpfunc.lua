8e8c72981b6c789f5cc9195b287cd48d881d45d3
disrecord <goncharovoi@yandex.ru>
2017-02-27 23:50:45 +0300

    Rename package, CMakeLists.txt small fixes.

--- motion/sweetie_bot_controllers_joint_space/scripts/helpfunc.lua
+++
@@ -1,7 +0,0 @@
-require 'rttlib'
---- Per property setter for complex types
-function setProperty(peer, prop, prop_type, value) 
-	var = rtt.Variable(prop_type);
-	var:fromtab(value)
-	peer:getProperty(prop):set(var)
-end
