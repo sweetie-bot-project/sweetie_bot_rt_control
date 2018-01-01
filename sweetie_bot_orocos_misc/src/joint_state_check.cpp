e186226ddabb18aab2f055495617a0889e9715a3
Oleg Goncharov <goncharovoi@yandex.ru>
2018-01-01 19:03:39 +0300

    Add isValidJointStatePosVel() function. Remove redundant checks.

--- sweetie_bot_orocos_misc/src/joint_state_check.cpp
+++ sweetie_bot_orocos_misc/src/joint_state_check.cpp
@@ -28,7 +28,6 @@ bool isValidJointStateNamePos(const sensor_msgs::JointState& msg, int sz)
 	if (sz < 0) sz = msg.name.size();
 	else if (sz != msg.name.size()) return false;
 	if (sz != msg.position.size()) return false;
-	if (sz != msg.position.size() && msg.position.size() != 0) return false;
 	if (sz != msg.velocity.size() && msg.velocity.size() != 0) return false;
 	return true;
 }
@@ -38,8 +37,16 @@ bool isValidJointStatePos(const sensor_msgs::JointState& msg, int sz)
 	if (sz < 0) sz = msg.position.size();
 	else if (sz != msg.position.size()) return false;
 	if (sz != msg.name.size() && msg.name.size() != 0) return false;
-	if (sz != msg.position.size() && msg.position.size() != 0) return false;
 	if (sz != msg.velocity.size() && msg.velocity.size() != 0) return false;
+	return true;
+}
+
+bool isValidJointStatePosVel(const sensor_msgs::JointState& msg, int sz)
+{
+	if (sz < 0) sz = msg.position.size();
+	else if (sz != msg.position.size()) return false;
+	if (sz != msg.name.size() && msg.name.size() != 0) return false;
+	if (sz != msg.velocity.size()) return false;
 	return true;
 }
 
