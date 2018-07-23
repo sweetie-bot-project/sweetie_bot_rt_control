08d99b67c25b55082177eaa4c0f8e45785ef77da
disrecord <goncharovoi@yandex.ru>
2018-07-23 14:12:02 +0300

    Add base_link twist visualization.

--- motion/sweetie_bot_dynamics/src/visualizer.cpp
+++ motion/sweetie_bot_dynamics/src/visualizer.cpp
@@ -59,6 +59,7 @@ class DynamicsVisualizer
 		ros::Subscriber joints_accel_sub;
 		ros::Subscriber supports_sub;
 		ros::Subscriber wrenches_sub;
+		ros::Subscriber base_sub;
 		ros::Subscriber balance_sub;
 		// tf
 		tf::TransformListener tf_listener;
@@ -79,6 +80,7 @@ class DynamicsVisualizer
 		// BUFFERS
 		SupportState supports;
 		RigidBodyState wrenches;
+		RigidBodyState base;
 		BalanceState balance;
 
 	public:
@@ -86,6 +88,7 @@ class DynamicsVisualizer
 		{
 			// input
 			joints_accel_sub = node_handler.subscribe<JointStateAccel>("joint_state_accel", 1, &DynamicsVisualizer::callbackJointsAccelSub, this);
+			base_sub = node_handler.subscribe<RigidBodyState>("base", 1, &DynamicsVisualizer::callbackBaseSub, this);
 			wrenches_sub = node_handler.subscribe<RigidBodyState>("wrenches", 1, &DynamicsVisualizer::callbackWrenchesSub, this);
 			supports_sub = node_handler.subscribe<SupportState>("supports", 1, &DynamicsVisualizer::callbackSupportsSub, this);
 			balance_sub = node_handler.subscribe<BalanceState>("balance", 1, &DynamicsVisualizer::callbackBalanceSub, this);
@@ -173,6 +176,14 @@ class DynamicsVisualizer
 			if (sweetie_bot::isValidRigidBodyStateNameFrame(*msg)) {
 				// buffer message for following vizualization
 				wrenches = *msg;
+			}
+		}
+
+		void callbackBaseSub(const RigidBodyState::ConstPtr& msg) 
+		{
+			if (sweetie_bot::isValidRigidBodyStateNameFrame(*msg, 1)) {
+				// buffer message for following vizualization
+				base = *msg;
 			}
 		}
 
@@ -278,19 +289,34 @@ class DynamicsVisualizer
 					marker.points.emplace_back(); tf::pointKDLToMsg(point_torque, marker.points.back()); marker.colors.push_back(BLUE);
 			}
 			if (display_twist) {
+				// display limbs twists
 				for(int k = 0; k < wrenches.twist.size(); k++) {
 					// get velocity and bring it to limb tip
 					KDL::Twist twist = wrenches.twist[k];
 					KDL::Vector point = wrenches.frame[k].p;
 					twist = twist.RefPoint(point);
 					// velocity visualization
-					KDL::Vector point_force = point + twist.vel; // linear vel visualization
-					KDL::Vector point_torque = point + twist.rot*velocity_angular_scale_param; // angular vel visualization
+					KDL::Vector point_vel = point + twist.vel; // linear vel visualization
+					KDL::Vector point_rot = point + twist.rot*velocity_angular_scale_param; // angular vel visualization
 					// add to line list
 					marker.points.emplace_back(); tf::pointKDLToMsg(point, marker.points.back()); marker.colors.push_back(GREEN);
-					marker.points.emplace_back(); tf::pointKDLToMsg(point_force, marker.points.back()); marker.colors.push_back(GREEN);
+					marker.points.emplace_back(); tf::pointKDLToMsg(point_vel, marker.points.back()); marker.colors.push_back(GREEN);
 					marker.points.emplace_back(); tf::pointKDLToMsg(point, marker.points.back()); marker.colors.push_back(YELLOW);
-					marker.points.emplace_back(); tf::pointKDLToMsg(point_torque, marker.points.back()); marker.colors.push_back(YELLOW);
+					marker.points.emplace_back(); tf::pointKDLToMsg(point_rot, marker.points.back()); marker.colors.push_back(YELLOW);
+				}
+				// display base twist
+				if (base.name.size() == 1) {
+					KDL::Twist twist = base.twist[0];
+					KDL::Vector point = base.frame[0].p;
+					twist = twist.RefPoint(point);
+					// velocity visualization
+					KDL::Vector point_vel = point + twist.vel; // linear vel visualization
+					KDL::Vector point_rot = point + twist.rot*velocity_angular_scale_param; // angular vel visualization
+					// add to line list
+					marker.points.emplace_back(); tf::pointKDLToMsg(point, marker.points.back()); marker.colors.push_back(GREEN);
+					marker.points.emplace_back(); tf::pointKDLToMsg(point_vel, marker.points.back()); marker.colors.push_back(GREEN);
+					marker.points.emplace_back(); tf::pointKDLToMsg(point, marker.points.back()); marker.colors.push_back(YELLOW);
+					marker.points.emplace_back(); tf::pointKDLToMsg(point_rot, marker.points.back()); marker.colors.push_back(YELLOW);
 				}
 			}
 			// publish resulting message
