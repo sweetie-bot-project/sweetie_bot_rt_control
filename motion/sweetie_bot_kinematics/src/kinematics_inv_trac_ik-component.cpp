410e33f43f93f7462a12c7c5ab27c04c816d94ad
disrecord <goncharovoi@yandex.ru>
2018-12-03 00:07:40 +0300

    Third level fix for TRAC_IK bug with position outside joints limits.

--- motion/sweetie_bot_kinematics/src/kinematics_inv_trac_ik-component.cpp
+++ motion/sweetie_bot_kinematics/src/kinematics_inv_trac_ik-component.cpp
@@ -218,7 +218,13 @@ bool KinematicsInvTracIK::poseToJointState_impl(const sweetie_bot_kinematics_msg
 			return false;
 		}
 		// fix trak_ik bug: map angles to [-pi, pi] interval. TODO: bug report.
-		std::transform( chain_it->jnt_array_pose.data.data(), chain_it->jnt_array_pose.data.data() + chain_it->size,  chain_it->jnt_array_pose.data.data(), [](double angle) { return std::fmod(angle, M_PI); } );
+		std::transform( chain_it->jnt_array_pose.data.data(), chain_it->jnt_array_pose.data.data() + chain_it->size,  chain_it->jnt_array_pose.data.data(), 
+				[](double angle) {
+					angle = std::fmod(angle + M_PI, 2*M_PI);
+					angle = (angle >= 0.0) ? angle : angle + 2*M_PI;
+					return angle - M_PI;
+				}
+			);
 
 		// use computed pose as new seed
 		if (use_ik_pose_as_new_seed_) chain_it->jnt_array_seed_pose = chain_it->jnt_array_pose;
