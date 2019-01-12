036de41b347c96d4bcd4f08c989c33cc062d91aa
disrecord <goncharovoi@yandex.ru>
2019-01-12 14:15:15 +0300

    Add ExecuteStepSequence controller.

---
+++ motion/controllers/sweetie_bot_controller_cartesian/src/cartesian_trajectory_cache.cpp
@@ -0,0 +1,208 @@
+#include "cartesian_trajectory_cache.hpp"
+
+#include <Eigen/Geometry> 
+#include <kdl_conversions/kdl_msg.h>
+#include <sweetie_bot_robot_model/robot_model.hpp>
+
+namespace sweetie_bot {
+namespace motion {
+namespace controller {
+
+static double angularDistance(const KDL::Rotation& rot, const geometry_msgs::Quaternion& quat) 
+{
+	Eigen::Quaterniond w1;
+	rot.GetQuaternion(w1.x(), w1.y(), w1.z(), w1.w());
+	Eigen::Quaterniond w2(quat.x, quat.y, quat.z, quat.w);
+	w2.normalize();
+	return abs(w1.angularDistance(w2));
+}
+
+static double angularDistance(const KDL::Rotation& rot1, const KDL::Rotation& rot2) 
+{
+	Eigen::Quaterniond w1, w2;
+	rot1.GetQuaternion(w1.x(), w1.y(), w1.z(), w1.w());
+	rot2.GetQuaternion(w2.x(), w2.y(), w2.z(), w2.w());
+	return w1.angularDistance(w2);
+}
+
+static double distance2(const KDL::Vector& v1, const geometry_msgs::Point v2) {
+	return (v1.x() - v2.x)*(v1.x() - v2.x) + (v1.y() - v2.y)*(v1.y() - v2.y) + (v1.z() - v2.z)*(v1.z() - v2.z);
+}
+
+static double distance(const KDL::Vector& v1, const geometry_msgs::Point& v2) {
+	return std::sqrt(distance2(v1, v2));
+}
+
+static double distance(const KDL::Vector& v1, const KDL::Vector& v2) {
+	return (v1 - v2).Norm();
+}
+
+/*static bool CartesianTrajectoryCache::checkRigidBodyTrajectoryPosVelContact(const RigidBodyTrajectory& msg, int n) 
+{
+	if (msg.position.size() != n) return false;
+	if (msg.orientation.size() != n) return false;
+	if (msg.velocity.size() != n) return false;
+	if (msg.contact.size() != n) return false;
+	return true;
+}*/
+	
+
+CartesianTrajectoryCache::CartesianTrajectoryCache(FollowStepSequenceGoal_const_shared_ptr goal, RobotModel * robot_model, double period)
+{
+	if (!robot_model || !robot_model->ready()) throw std::invalid_argument("CartesianTrajectoryCache: RobotModel is not ready");
+
+	// append flag is not supported
+	if (goal->append) {
+		throw std::invalid_argument("CartesianTrajectoryCache: append flag is not supported.");
+	}
+	// get number of end effectors
+	int n_chains = goal->ee_motion.size();
+	if (n_chains < 1) {
+		throw std::invalid_argument("CartesianTrajectoryCache: at least one end effector must be specified.");
+	}
+	// get number of points
+	int n_points = goal->time_from_start.size();;
+	if (n_points < 2) {
+		throw std::invalid_argument("CartesianTrajectoryCache: trajectory must contain at least two points.");
+	}
+	// check if sizes are consistent
+	if (goal->base_motion.points.size() != 0 && goal->base_motion.points.size() != n_points) {
+		throw std::invalid_argument("CartesianTrajectoryCache: incorrect number of points in base motion.");
+	}
+	// process chains
+	this->limb_index_fullpose.reserve(n_chains);
+	this->contact_name.reserve(n_chains);
+	for (int k = 0; k < n_chains; k++) {
+		// check chain size
+		if (goal->ee_motion[k].points.size() != n_points) {
+			throw std::invalid_argument("CartesianTrajectoryCache: incorrect number of points in ee motion.");
+		}
+		// chains must be registered in robot model
+		int index = robot_model->getChainIndex(goal->ee_motion[k].name);
+		if (index < 0) {
+			throw std::out_of_range("CartesianTrajectoryCache: unknown kinematic chain name: " + goal->ee_motion[k].name);
+		}
+		this->limb_index_fullpose.push_back(index);
+		// save default contact names
+		this->contact_name.push_back(robot_model->getChainDefaultContact(goal->ee_motion[k].name));
+	}
+	// time intervals must be equal to period
+	for(int k = 0; k < n_points; k++) {
+		if (goal->time_from_start[k] != period*k) { 
+			throw std::invalid_argument("CartesianTrajectoryCache: period is not equal to component period.");
+		}
+	}
+
+	// state initialization
+	this->step_sequence_list.push_back(goal);
+	this->time_step = 0;
+}
+
+
+bool CartesianTrajectoryCache::step() 
+{
+	// check current trajectory chunk is not finished
+	if (time_step < step_sequence_list.front()->time_from_start.size() - 1) {
+		time_step++;
+		return true;
+	}
+	// check if next chunk is available 
+	if (step_sequence_list.size() > 1) {
+		step_sequence_list.pop_front();
+		time_step = 0;
+		return true;
+	}
+	// do not touch marker
+	return false;
+}
+
+void CartesianTrajectoryCache::prepareEndEffectorStateBuffer(RigidBodyState& limbs) const
+{
+	const FollowStepSequenceGoal& msg = *step_sequence_list.front();
+	int n_limbs = msg.ee_motion.size();
+	limbs.name.clear(); 
+	limbs.name.reserve(n_limbs);
+	for(int k = 0; k < n_limbs; k++) limbs.name.push_back(msg.ee_motion[k].name);
+	limbs.frame.resize(n_limbs);
+	limbs.twist.resize(n_limbs);
+	limbs.wrench.clear();
+}
+
+std::vector<std::string> CartesianTrajectoryCache::getRequiredChains() const 
+{
+	const FollowStepSequenceGoal& msg = *step_sequence_list.front();
+	std::vector<std::string> names;
+	names.reserve( msg.ee_motion.size() );
+	for(const auto& ee : msg.ee_motion) names.push_back(ee.name);
+}
+
+void CartesianTrajectoryCache::getEndEffectorState(RigidBodyState& limbs) const
+{
+	const FollowStepSequenceGoal& msg = *step_sequence_list.front();
+	int n_limbs = msg.ee_motion.size();
+	for(int k = 0; k < n_limbs; k++) {
+		tf::poseMsgToKDL(msg.ee_motion[k].points[time_step].pose, limbs.frame[k]);
+		limbs.twist[k] = msg.ee_motion[k].points[time_step].twist;
+	}
+}
+
+int CartesianTrajectoryCache::checkPathToleranceFullpose(const RigidBodyState& limbs_full) const 
+{
+	const FollowStepSequenceGoal& msg = *step_sequence_list.front();
+	int n_limbs = msg.ee_motion.size();
+	// check end_effector pose: both poses are presented in base_link frame
+	for(int k = 0; k < n_limbs; k++) {
+		if ( distance( limbs_full.frame[limb_index_fullpose[k]].p, msg.ee_motion[k].points[time_step].pose.position ) > msg.position_tolerance ) return limb_index_fullpose[k];
+		if ( angularDistance( limbs_full.frame[limb_index_fullpose[k]].M, msg.ee_motion[k].points[time_step].pose.orientation ) > msg.orientation_tolerance ) return limb_index_fullpose[k];
+	}
+	return -1;	
+}	
+	
+	
+int CartesianTrajectoryCache::checkPathTolerance(const RigidBodyState& limbs_real, const RigidBodyState& limbs_desired) const
+{
+	const FollowStepSequenceGoal& msg = *step_sequence_list.front();
+	int n_limbs = msg.ee_motion.size();
+	// check end_effector pose: both poses are presented in base_link frame
+	for(int k = 0; k < n_limbs; k++) {
+		if ( distance( limbs_real.frame[k].p, limbs_desired.frame[k].p ) > msg.position_tolerance ) return k;
+		if ( angularDistance( limbs_real.frame[k].M, limbs_desired.frame[k].M ) > msg.orientation_tolerance ) return k;
+	}
+	return -1;	
+}
+
+void CartesianTrajectoryCache::selectEndEffector(const RigidBodyState& in_sorted, RigidBodyState& out) const
+{
+	const FollowStepSequenceGoal& msg = *step_sequence_list.front();
+	int n_limbs = msg.ee_motion.size();
+	for(int k = 0; k < n_limbs; k++) {
+		out.frame[k] = in_sorted.frame[limb_index_fullpose[k]];
+		out.twist[k] = in_sorted.twist[limb_index_fullpose[k]];
+	}	
+}
+		
+
+void CartesianTrajectoryCache::prepareSupportStateBuffer(SupportState& support) const
+{
+	const FollowStepSequenceGoal& msg = *step_sequence_list.front();
+	int n_limbs = msg.ee_motion.size();
+	support.name.clear(); 
+	support.name.reserve(n_limbs);
+	for(int k = 0; k < n_limbs; k++) support.name.push_back(msg.ee_motion[k].name);
+	support.contact = contact_name;
+	support.support.resize(n_limbs);
+}
+
+void CartesianTrajectoryCache::getSupportState(SupportState& support) const
+{
+	const FollowStepSequenceGoal& msg = *step_sequence_list.front();
+	int n_limbs = msg.ee_motion.size();
+	for(int k = 0; k < n_limbs; k++) {
+		support.support[k] = msg.ee_motion[k].points[time_step].contact ? 1.0 : 0.0;
+	}
+}
+
+} // namespace controller
+} // namespace motion
+} // namespace sweetie_bot
+
---
+++ motion/controllers/sweetie_bot_controller_cartesian/src/cartesian_trajectory_cache.hpp
@@ -0,0 +1,134 @@
+#ifndef  JOINT_TRAJECTORY_CACHE_HPP
+#define  JOINT_TRAJECTORY_CACHE_HPP
+
+#include <sensor_msgs/typekit/JointState.h>
+#include <sweetie_bot_kinematics_msgs/typekit/FollowStepSequenceAction.h>
+#include <sweetie_bot_kinematics_msgs/typekit/SupportState.h>
+#include <sweetie_bot_kinematics_msgs/typekit/RigidBodyState.h>
+
+namespace sweetie_bot {
+namespace motion {
+
+class RobotModel;
+
+namespace controller {
+
+
+/**
+ * @brief CartesianRobotState cache. 
+ * Store supplied messages "as is" using shared pointer. Object includes time marker.
+ *
+ * TODO In future versions should also support:
+ * * messages appending
+ * * hermite interpolation if period does not match
+ * * restore velocity by spline 
+ *
+ * TODO base link processing, cahin index access, limb name vector bufferizaion
+ **/
+class CartesianTrajectoryCache 
+{
+	public:
+		typedef sweetie_bot_kinematics_msgs::RigidBodyState RigidBodyState;
+		typedef sweetie_bot_kinematics_msgs::FollowStepSequenceGoal FollowStepSequenceGoal;
+		typedef sweetie_bot_kinematics_msgs::SupportState SupportState;
+		typedef boost::shared_ptr<const FollowStepSequenceGoal> FollowStepSequenceGoal_const_shared_ptr;
+		typedef std::list<FollowStepSequenceGoal_const_shared_ptr> StepSequenceList;
+
+	protected:
+		StepSequenceList step_sequence_list; /**< Contains trajectory chunks. */
+		int time_step; /**< Contains number of point in step_sequence_list.front(). Always valid. */
+		std::vector<int> limb_index_fullpose; /**< index of coresponding chain in fullpose */
+		std::vector<std::string> contact_name; /**< buffers default contact names from robot model */
+
+	public:
+		/**
+		 * @brief Create new trajectory from sweetie_bot_kinematics_msgs::FollowStepSequenceGoal message.
+		 * Check message consistency and throw std::invalid_argument if check failed.
+		 * @param trajectory Pointer to goal message. Shared pointer is used to minimize number of copy operations.
+		 * @param robot_model Pointer to RobotModel.
+		 * @param period Discretizaion period of trajectory.
+		 **/
+		CartesianTrajectoryCache(FollowStepSequenceGoal_const_shared_ptr trajectory, RobotModel * robot_model, double period);
+
+		
+		/**
+		 * @brief Move time marker one period in future.
+		 * Move time marker one period ahead.
+		 * @return false if trajectory is finished
+		 **/
+		bool step();
+
+		/**
+		 * @brief Prepare message buffers.
+		 * Set names, resize arrays.
+		 * @param limbs Buffer to be prepared.
+		 **/
+		void prepareEndEffectorStateBuffer(RigidBodyState& limbs) const;
+
+		/**
+		 * @brief Return lists of required kinematics chains (according to RobotModel).
+		 * @return List of kinematics chains.
+		 **/
+		std::vector<std::string> getRequiredChains() const;
+
+		/**
+		 * @brief Return lists of required kinematics chains (according to RobotModel).
+		 * @return List of kinematics chains.
+		 **/
+		double getTime() const {
+			return step_sequence_list.front()->time_from_start[time_step];
+		}
+
+		/**
+		 * @brief Current point of trajectory in buffer of proper size.
+		 * Method does not check buffer size. 
+		 * @param limbs RigidBodyState buffer receiving new state represented in base link frame.
+		 * @return true if time @a t in range of trajectory.
+		 **/
+		void getEndEffectorState(RigidBodyState& limbs) const;
+
+		/*
+		 * @brief Simplifies check of start conditions: check path tolerance at current time.
+		 * @param joints_real_sorted  Full pose in joint space sorted according to RobotModel.
+		 * @return index of kinematic chain which violates tolerance, -1 otherwise
+		 **/
+		int checkPathToleranceFullpose(const RigidBodyState& limbs_real_sorted) const;
+
+		/*
+		 * @brief Check if given pose is within defined path tolerance.
+		 * Check path_tolerance. Both poses must have the same layout as trajectory.
+		 * @return index of kinematic chain which violates tolerance, -1 otherwise
+		 **/
+		int checkPathTolerance(const RigidBodyState& joints_real, const RigidBodyState& joints_desired) const;
+
+		/*
+		 * @brief Select kinematic chains from full robot pose. 
+		 * Method does  not perform size checks and does not set kinematic chains' names. Use @c prepareEndEffectorStateBuffer() to prepare it.
+		 * @param in_sorted Full pose  sorted according to RobotModel.
+		 * @param out Selected kinematic chains.
+		 **/
+		void selectEndEffector(const RigidBodyState& in_sorted, RigidBodyState& out) const;
+
+		/**
+		 * @brief Prepare SupportState message.
+		 * Resize arrays, set names.
+		 * @param support message buffer,
+		 **/
+		void prepareSupportStateBuffer(SupportState& support) const;
+
+		/**
+		 * @brief Get supports' state to buffer of proper size.
+		 * Method does not check message
+		 * @param supports Support state buffer receiving new state
+		 **/
+		void getSupportState(SupportState& support) const;
+};
+
+
+} // namespace controller
+} // namespace motion
+} // namespace sweetie_bot
+
+
+#endif  /*JOINT_TRAJECTORY_CACHE_HPP*/
+
