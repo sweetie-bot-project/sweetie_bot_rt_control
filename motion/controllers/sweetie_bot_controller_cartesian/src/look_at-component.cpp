c0648260817e6b7674abd36d19868a721182603b
disrecord <goncharovoi@yandex.ru>
2019-10-08 12:44:00 +0300

    LookAt controller publishes target pitch and yaw for controlling eyes animation.

--- motion/controllers/sweetie_bot_controller_cartesian/src/look_at-component.cpp
+++ motion/controllers/sweetie_bot_controller_cartesian/src/look_at-component.cpp
@@ -41,18 +41,22 @@ LookAt::LookAt(std::string const& name)  :
 {
 	// ports input 
 	this->addPort("in_limbs", in_limbs_port)
-		.doc("Robot limbs positions.");
+		.doc("Robot limbs positions in base_link frame.");
 	this->addPort("in_base", in_base_port)
-		.doc("Robot base link pose in world frame.");
+		.doc("Robot base_link pose in world frame.");
 	this->addPort("in_pose_ref", in_pose_ref_port)
-		.doc("Kinematic chain last link would be oriented in such way that x-axis points to position in_pose_ref and z-axis points up.");
+		.doc("Target pose in world frame. Robot should look in derection specified by its position.");
 	// ports input 
 	this->addPort("out_limbs_ref", out_limbs_ref_port)
-		.doc("Limb next position to achieve target robot state. It is computed by component each control cycle. ");
+		.doc("Limb reference position. It is updated by component each control cycle to change limb pose that its last link x-axis points to  in_pose_ref position and z-axis is oriented upward. ");
 	this->addPort("out_supports", out_supports_port)
-		.doc("Active contact list. Controlled chains are free.");
+		.doc("Active contact list. Controlled kineamtic chain is assumed to be free.");
+	this->addPort("out_joints_ref", out_joints_ref_port)
+		.doc("Reference joints state for eyes animation. It publish pitch and yaw of target pose in the frame of the last link of controlled kineamtic chain. Published joints' name is set via `pitch_yaw_joints`.");
 
 	// properties
+	this->addProperty("pitch_yaw_joints", pitch_yaw_joints)
+		.doc("Names of pitch and yaw joints which represents \"eyes\" position. Must be empty or contain two elements. ");
 	// operations: provided
 	// operations: required
 	this->requires()->addOperationCaller(poseToJointStatePublish); // kinematics service
@@ -68,11 +72,8 @@ bool LookAt::configureHook_impl()
 {
 	// INITIALIZATION
 	// check if filter present
-	filter = getSubServiceByType<filter::FilterRigidBodyStateInterface>(this->provides().get());
-	/*if (!filter) {
-		log(ERROR) << "RigidBodyState filter service is not loaded." << endlog();
-		return false;
-	}*/
+	filter_pose = getSubServiceByType<filter::FilterRigidBodyStateInterface>(this->provides().get());
+	filter_joints = getSubServiceByType<filter::FilterJointStateInterface>(this->provides().get());
 
 	// check if RobotModel Service presents
 	if (!robot_model->ready() || !robot_model->isConfigured()) {
@@ -88,7 +89,20 @@ bool LookAt::configureHook_impl()
 		log(INFO) << "poseToJointStatePublish operation is disconnected. Asynchronous IK interface via port is used." << endlog();
 	}
 
+	// check eyes animation pitch and yaw property
+	if (pitch_yaw_joints.size() > 0) {
+		if (pitch_yaw_joints.size() != 2) {
+			log(ERROR) << "'pitch_yaw_joints' must contains zero or two elements." << endlog();
+			return false;
+		}
+		if (robot_model->getJointIndex(pitch_yaw_joints[0]) < 0 || robot_model->getJointIndex(pitch_yaw_joints[1]) < 0) {
+			log(ERROR) << "Unknown joint in 'pitch_yaw_joints' property." << endlog();
+			return false;
+		}
+	}
+
 	// allocate memory
+	// cartesian state
 	limb_next.name.resize(1);
 	limb_next.frame.resize(1);
 	limb_next.twist.resize(1);
@@ -98,10 +112,17 @@ bool LookAt::configureHook_impl()
 	supports.name.resize(1);
 	supports.contact.assign(1, "");
 	supports.support.assign(1, 0.0);
+	// joints state
+	int n_joints = pitch_yaw_joints.size();
+	joints_ref.name = pitch_yaw_joints;
+	joints_ref.position.assign(n_joints, 0.0);
+	joints_ref.velocity.assign(n_joints, 0.0);
+	joints_next = joints_ref;
 
 	// data samples
 	out_supports_port.setDataSample(supports);
 	out_limbs_ref_port.setDataSample(limb_next);
+	out_joints_ref_port.setDataSample(joints_next);
 
 	log(INFO) << "LookAt is configured !" << endlog();
 	return true;
@@ -162,8 +183,16 @@ bool LookAt::resourceChangedHook_impl(const std::vector<std::string>& set_operat
 	// limb pose in world frame
 	limb_ref.frame[0] = base.frame[0]*limbs.frame[chain_index];
 	target_point = limb_ref.frame[0] * KDL::Vector(1.0, 0.0, 0.0);
+	target_position = limbs.frame[chain_index].p;
+	// joints to zero
+	joints_ref.position.assign(joints_ref.position.size(), 0.0);
+
 	// reset filter
-	if (filter && !filter->reset(limb_ref, period)) {
+	if (filter_pose && !filter_pose->reset(limb_ref, period)) {
+		log(ERROR) << "RigidBodyState filter reset has failed." << endlog();
+		return false;
+	}
+	if (filter_joints && !filter_joints->reset(joints_ref, period)) {
 		log(ERROR) << "RigidBodyState filter reset has failed." << endlog();
 		return false;
 	}
@@ -228,14 +257,14 @@ void LookAt::updateHook_impl()
 	}
 	// form desired frame
 	limb_ref.frame[0].M = KDL::Rotation(x_axis, y_axis, z_axis);
-	limb_ref.frame[0].p = ee_frame.p;
+	limb_ref.frame[0].p = base.frame[0] * target_position;
 
 	// 
 	// apply filter
 	//
-	if (filter) {
+	if (filter_pose) {
 		// apply filter to calculate next pose
-		filter->update(limb_next, limb_ref, limb_next);
+		filter_pose->update(limb_next, limb_ref, limb_next);
 		// return back to base_link frame and correct speed
 		limb_next.frame[0] = base.frame[0].Inverse()*limb_next.frame[0];
 		limb_next.twist[0] = base.frame[0].Inverse(limb_next.twist[0] - base.twist[0]);
@@ -264,6 +293,28 @@ void LookAt::updateHook_impl()
 
 	if (log(DEBUG)) {
 		log() << "ik_success = " << ik_success << " sync_mode = " << poseToJointStatePublish.ready() << " chain_index = "  << chain_index << endlog();
+	}
+
+	//
+	// calculate pitch and yaw for eyes if necessary
+	//
+	if (joints_ref.name.size() > 0) {
+		KDL::Vector tpos = limb_next.frame[0].Inverse(base.frame[0].Inverse(target_point)); // target position in base_link frame
+		double rxy = std::sqrt(tpos.x()*tpos.x() + tpos.y()*tpos.y());
+		joints_ref.position[0] = std::atan2(tpos.z(), rxy); // pitch
+		joints_ref.position[1] = -std::atan2(tpos.y(), tpos.x()); // yaw
+		// apply joint filter
+		if (filter_joints) {
+			// apply filter to calculate next pose and publish result
+			filter_joints->update(joints_next, joints_ref, joints_next);
+			joints_next.header.stamp = ros::Time::now();
+			out_joints_ref_port.write(joints_next);
+		}
+		else {
+			// publish reference pose
+			joints_ref.header.stamp = ros::Time::now();
+			out_joints_ref_port.write(joints_ref);
+		}
 	}
 }
 
--- motion/controllers/sweetie_bot_controller_cartesian/src/look_at-component.hpp
+++ motion/controllers/sweetie_bot_controller_cartesian/src/look_at-component.hpp
@@ -10,6 +10,7 @@
 #include <std_srvs/SetBool.h>
 #include <sensor_msgs/typekit/JointState.h>
 #include <geometry_msgs/typekit/PoseStamped.h>
+#include <sensor_msgs/typekit/JointState.h>
 #include <sweetie_bot_kinematics_msgs/typekit/RigidBodyState.h>
 #include <sweetie_bot_kinematics_msgs/typekit/SupportState.h>
 
@@ -17,6 +18,7 @@
 #include <sweetie_bot_resource_control/resource_client.hpp>
 #include <sweetie_bot_robot_model/robot_model.hpp>
 #include <sweetie_bot_controller_cartesian/filter_rigid_body_state.hpp>
+#include <sweetie_bot_controller_joint_space/filter_joint_state.hpp>
 
 #include <sweetie_bot_resource_control/simple_controller_base.hpp>
 
@@ -37,8 +39,10 @@ class LookAt : public SimpleControllerBase
 		// PORTS: output
 		RTT::OutputPort<sweetie_bot_kinematics_msgs::RigidBodyState> out_limbs_ref_port;
 		RTT::OutputPort<sweetie_bot_kinematics_msgs::SupportState> out_supports_port;
+		RTT::OutputPort<sensor_msgs::JointState> out_joints_ref_port;
 		// PROPERTIES
 		std::string chain_name;
+		std::vector< std::string > pitch_yaw_joints;
 
 	protected:
 		// OPERATIONS: provides
@@ -48,18 +52,22 @@ class LookAt : public SimpleControllerBase
 		// SERVICES: required
 		sweetie_bot::motion::RobotModel * robot_model;
 		// SERVICES: internal interface
-		sweetie_bot::motion::filter::FilterRigidBodyStateInterface * filter; // trajectory smoother
+		sweetie_bot::motion::filter::FilterRigidBodyStateInterface * filter_pose; // trajectory smoother
+		sweetie_bot::motion::filter::FilterJointStateInterface * filter_joints; // trajectory smoother
 
 	protected:
 		// COMPONENT STATE
 		int chain_index;
-		// ports buffers
 		KDL::Vector target_point; // point where x-axis should points
+		KDL::Vector target_position; // preferable position of origin of last_link frame in base_link frame (set on component activation, prevent last link positon change)
+		// ports buffers
 		sweetie_bot_kinematics_msgs::RigidBodyState limbs; // limb current pose
 		sweetie_bot_kinematics_msgs::RigidBodyState limb_ref; // desired limb pose
 		sweetie_bot_kinematics_msgs::RigidBodyState limb_next; // next pose calucalated by component
 		sweetie_bot_kinematics_msgs::RigidBodyState base; // base_link pose
 		sweetie_bot_kinematics_msgs::SupportState supports;
+		sensor_msgs::JointState joints_ref; // desired joints pose
+		sensor_msgs::JointState joints_next; // new joints pose, calculated by component
 		
 	public:
 		LookAt(std::string const& name);
