48d3d45a43902a105c9714cf23e9efc11eb3ed8f
disrecord <goncharovoi@yandex.ru>
2020-03-05 14:39:18 +0300

    Remove common and joints_diff packages with unnecessary experimental code.

--- motion/sweetie_bot_joints_diff/scripts/prepare_data.bash
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cat input.out | sed 's/(time)/ /g' | sed 's/servo1/ /g' | sed 's/servo2/ /g' | tr -s " " > B.out
-cat output.out | sed 's/(time)/ /g' | sed 's/servo1/ /g' | sed 's/servo2/ /g' | tr -s " " > A.out
-
-
