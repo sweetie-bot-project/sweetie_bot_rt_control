f1629b48edb937c1a3dd61608460a659caed34b1
disrecord <goncharovoi@yandex.ru>
2022-06-05 21:55:09 +0300

    Fix identation: spaces to tabs.

--- motion/sweetie_bot_robot_model/include/sweetie_bot_robot_model/robot_model.hpp
+++ motion/sweetie_bot_robot_model/include/sweetie_bot_robot_model/robot_model.hpp
@@ -13,43 +13,43 @@ namespace motion {
 
 class RobotModelInterface
 {
-    public:
+	public:
 		// configuartion	   
-        virtual bool isConfigured() = 0;
-        virtual bool configure() = 0;
-        virtual void cleanup() = 0;
+		virtual bool isConfigured() = 0;
+		virtual bool configure() = 0;
+		virtual void cleanup() = 0;
 
 		// robot description
-        virtual const std::string& getRobotDescription() const = 0;
-        virtual KDL::Chain getKDLChain(const std::string& name, bool with_virtual_joints) const = 0;
-        virtual KDL::Tree getKDLTree() const = 0;
+		virtual const std::string& getRobotDescription() const = 0;
+		virtual KDL::Chain getKDLChain(const std::string& name, bool with_virtual_joints) const = 0;
+		virtual KDL::Tree getKDLTree() const = 0;
 
 		// kinematic chains
-        virtual std::vector<std::string> listChains() const = 0;
-        virtual int getChainIndex(const std::string& chain) const = 0;
-        virtual std::vector<std::string> getChainJoints(const std::string& chain) const = 0;
-        virtual std::vector<int> getChainJointsInduces(const std::string& chain, bool with_virtual_joints) const = 0;
+		virtual std::vector<std::string> listChains() const = 0;
+		virtual int getChainIndex(const std::string& chain) const = 0;
+		virtual std::vector<std::string> getChainJoints(const std::string& chain) const = 0;
+		virtual std::vector<int> getChainJointsInduces(const std::string& chain, bool with_virtual_joints) const = 0;
 		virtual std::string getChainDefaultContact(const std::string& chain) const = 0;
 		virtual std::string getChainProperty(const std::string& name, const std::string& property) = 0;
-        virtual std::string getChainGroup(const std::string& chain) const = 0;
-        virtual std::vector<std::string> getChainsGroups(const std::vector<std::string>& chains) const = 0;
-        virtual KDL::JntArray getChainLowerLimits(const std::string& name) const = 0;
-        virtual KDL::JntArray getChainUpperLimits(const std::string& name) const = 0;
+		virtual std::string getChainGroup(const std::string& chain) const = 0;
+		virtual std::vector<std::string> getChainsGroups(const std::vector<std::string>& chains) const = 0;
+		virtual KDL::JntArray getChainLowerLimits(const std::string& name) const = 0;
+		virtual KDL::JntArray getChainUpperLimits(const std::string& name) const = 0;
 
 		// joint groups
-        virtual std::vector<std::string> listGroups() const = 0;
-        virtual int getGroupIndex(const std::string& group) const = 0;
-        virtual std::vector<std::string> getGroupJoints(const std::string& group) const = 0;
-        virtual std::vector<std::string> getGroupChains(const std::string& group) const = 0;
-        virtual std::vector<std::string> getGroupsChains(const std::vector<std::string>& groups) const = 0;
+		virtual std::vector<std::string> listGroups() const = 0;
+		virtual int getGroupIndex(const std::string& group) const = 0;
+		virtual std::vector<std::string> getGroupJoints(const std::string& group) const = 0;
+		virtual std::vector<std::string> getGroupChains(const std::string& group) const = 0;
+		virtual std::vector<std::string> getGroupsChains(const std::vector<std::string>& groups) const = 0;
 
 		// joints
-        virtual std::vector<std::string> listJoints() const = 0;
-        virtual int getJointIndex(const std::string& joint) const = 0;
-        virtual std::string getJointGroup(const std::string& joint) const = 0;
-        virtual std::vector<std::string> getJointsGroups(const std::vector<std::string>& joints) = 0;
-        virtual double getJointLowerLimit(const std::string& name) const = 0;
-        virtual double getJointUpperLimit(const std::string& name) const = 0;
+		virtual std::vector<std::string> listJoints() const = 0;
+		virtual int getJointIndex(const std::string& joint) const = 0;
+		virtual std::string getJointGroup(const std::string& joint) const = 0;
+		virtual std::vector<std::string> getJointsGroups(const std::vector<std::string>& joints) = 0;
+		virtual double getJointLowerLimit(const std::string& name) const = 0;
+		virtual double getJointUpperLimit(const std::string& name) const = 0;
 
 		// contacts
 		virtual std::vector<std::string> listContacts() const = 0;
@@ -58,121 +58,121 @@ class RobotModelInterface
 };
 
 class RobotModel : public RTT::ServiceRequester {
-    public:
+	public:
 		// configuration
-        RTT::OperationCaller<bool()> configure;
-        RTT::OperationCaller<bool()> isConfigured;
+		RTT::OperationCaller<bool()> configure;
+		RTT::OperationCaller<bool()> isConfigured;
 
 		// robot description
-        RTT::OperationCaller<const std::string&()> getRobotDescription;
-        RTT::OperationCaller<KDL::Chain(const std::string&, bool)> getKDLChain;
-        RTT::OperationCaller<KDL::Tree()> getKDLTree;
+		RTT::OperationCaller<const std::string&()> getRobotDescription;
+		RTT::OperationCaller<KDL::Chain(const std::string&, bool)> getKDLChain;
+		RTT::OperationCaller<KDL::Tree()> getKDLTree;
 
 		// kinematic chains
-        RTT::OperationCaller<std::vector<std::string>()> listChains;
-        RTT::OperationCaller<int(const std::string&)> getChainIndex;
-        RTT::OperationCaller<std::vector<std::string>(const std::string&)> getChainJoints;
-        RTT::OperationCaller<std::vector<int>(const std::string&, bool)> getChainJointsInduces;
+		RTT::OperationCaller<std::vector<std::string>()> listChains;
+		RTT::OperationCaller<int(const std::string&)> getChainIndex;
+		RTT::OperationCaller<std::vector<std::string>(const std::string&)> getChainJoints;
+		RTT::OperationCaller<std::vector<int>(const std::string&, bool)> getChainJointsInduces;
 		RTT::OperationCaller<std::string(const std::string&)> getChainDefaultContact;
 		RTT::OperationCaller<std::string(const std::string&, const std::string&)> getChainProperty;
-        RTT::OperationCaller<std::string(const std::string&)> getChainGroup;
-        RTT::OperationCaller<std::vector<std::string>(const std::vector<std::string>&)> getChainsGroups;
-        RTT::OperationCaller<KDL::JntArray(const std::string&)> getChainLowerLimits;
-        RTT::OperationCaller<KDL::JntArray(const std::string&)> getChainUpperLimits;
+		RTT::OperationCaller<std::string(const std::string&)> getChainGroup;
+		RTT::OperationCaller<std::vector<std::string>(const std::vector<std::string>&)> getChainsGroups;
+		RTT::OperationCaller<KDL::JntArray(const std::string&)> getChainLowerLimits;
+		RTT::OperationCaller<KDL::JntArray(const std::string&)> getChainUpperLimits;
 
 		// joint groups
-        RTT::OperationCaller<std::vector<std::string>()> listGroups;
-        RTT::OperationCaller<int(const std::string&)> getGroupIndex;
-        RTT::OperationCaller<std::vector<std::string>(const std::string&)> getGroupJoints;
-        RTT::OperationCaller<std::vector<std::string>(const std::string&)> getGroupChains;
-        RTT::OperationCaller<std::vector<std::string>(const std::vector<std::string>&)> getGroupsChains;
+		RTT::OperationCaller<std::vector<std::string>()> listGroups;
+		RTT::OperationCaller<int(const std::string&)> getGroupIndex;
+		RTT::OperationCaller<std::vector<std::string>(const std::string&)> getGroupJoints;
+		RTT::OperationCaller<std::vector<std::string>(const std::string&)> getGroupChains;
+		RTT::OperationCaller<std::vector<std::string>(const std::vector<std::string>&)> getGroupsChains;
 
 		// joints
-        RTT::OperationCaller<std::vector<std::string>()> listJoints;
-        RTT::OperationCaller<int(const std::string&)> getJointIndex;
-        RTT::OperationCaller<std::string(const std::string&)> getJointGroup;
-        RTT::OperationCaller<std::vector<std::string>(const std::vector<std::string>&)> getJointsGroups;
-        RTT::OperationCaller<double(const std::string&)> getJointLowerLimit;
-        RTT::OperationCaller<double(const std::string&)> getJointUpperLimit;
+		RTT::OperationCaller<std::vector<std::string>()> listJoints;
+		RTT::OperationCaller<int(const std::string&)> getJointIndex;
+		RTT::OperationCaller<std::string(const std::string&)> getJointGroup;
+		RTT::OperationCaller<std::vector<std::string>(const std::vector<std::string>&)> getJointsGroups;
+		RTT::OperationCaller<double(const std::string&)> getJointLowerLimit;
+		RTT::OperationCaller<double(const std::string&)> getJointUpperLimit;
 
 		// contacts
 		RTT::OperationCaller<std::vector<std::string> ()> listContacts;
 		RTT::OperationCaller<std::vector<KDL::Vector> (const std::string&)> getContactPoints;
 		RTT::OperationCaller<int (const std::string&, std::vector<KDL::Vector>&)> addContactPointsToBuffer;
 
-        RobotModel(RTT::TaskContext * owner) :
-            RTT::ServiceRequester("robot_model", owner),
+		RobotModel(RTT::TaskContext * owner) :
+			RTT::ServiceRequester("robot_model", owner),
 			// configuration 
-            configure("configure"),
-            isConfigured("isConfigured"),
+			configure("configure"),
+			isConfigured("isConfigured"),
 			// robot description
-            getRobotDescription("getRobotDescription"),
-            getKDLChain("getKDLChain"),
-            getKDLTree("getKDLTree"),
+			getRobotDescription("getRobotDescription"),
+			getKDLChain("getKDLChain"),
+			getKDLTree("getKDLTree"),
 			// kinematic chains
-            listChains("listChains"),
-            getChainIndex("getChainIndex"),
+			listChains("listChains"),
+			getChainIndex("getChainIndex"),
 			getChainJoints("getChainJoints"),
 			getChainJointsInduces("getChainJointsInduces"),
-            getChainDefaultContact("getChainDefaultContact"),
+			getChainDefaultContact("getChainDefaultContact"),
 			getChainProperty("getChainProperty"),
 			getChainGroup("getChainGroup"),
 			getChainsGroups("getChainsGroups"),
-            getChainLowerLimits("getChainLowerLimits"),
-            getChainUpperLimits("getChainUpperLimits"),
+			getChainLowerLimits("getChainLowerLimits"),
+			getChainUpperLimits("getChainUpperLimits"),
 			// joint groups
-            listGroups("listGroups"),
-            getGroupIndex("getGroupIndex"),
+			listGroups("listGroups"),
+			getGroupIndex("getGroupIndex"),
 			getGroupJoints("getGroupJoints"),
 			getGroupChains("getGroupChains"),
 			getGroupsChains("getGroupsChains"),
 			// joints
-            listJoints("listJoints"),
-            getJointIndex("getJointIndex"),
-            getJointGroup("getJointGroup"),
-            getJointsGroups("getJointsGroups"),
-            getJointLowerLimit("getJointLowerLimit"),
-            getJointUpperLimit("getJointUpperLimit"),
+			listJoints("listJoints"),
+			getJointIndex("getJointIndex"),
+			getJointGroup("getJointGroup"),
+			getJointsGroups("getJointsGroups"),
+			getJointLowerLimit("getJointLowerLimit"),
+			getJointUpperLimit("getJointUpperLimit"),
 			// contacts 
 			listContacts("listContacts"),
 			getContactPoints("getContactPoints"),
 			addContactPointsToBuffer("addContactPointsToBuffer")
-        {
-            addOperationCaller(configure);
-            addOperationCaller(isConfigured);
+		{
+			addOperationCaller(configure);
+			addOperationCaller(isConfigured);
 			// robot description
 			addOperationCaller(getRobotDescription);
-            addOperationCaller(getKDLChain);
-            addOperationCaller(getKDLTree);
+			addOperationCaller(getKDLChain);
+			addOperationCaller(getKDLTree);
 			// kinematic chains
-            addOperationCaller(listChains);
-            addOperationCaller(getChainIndex);
+			addOperationCaller(listChains);
+			addOperationCaller(getChainIndex);
 			addOperationCaller(getChainJoints),
 			addOperationCaller(getChainJointsInduces),
-            addOperationCaller(getChainDefaultContact);
+			addOperationCaller(getChainDefaultContact);
 			addOperationCaller(getChainProperty);
-            addOperationCaller(getChainGroup);
-            addOperationCaller(getChainsGroups);
-            addOperationCaller(getChainLowerLimits);
-            addOperationCaller(getChainUpperLimits);
+			addOperationCaller(getChainGroup);
+			addOperationCaller(getChainsGroups);
+			addOperationCaller(getChainLowerLimits);
+			addOperationCaller(getChainUpperLimits);
 			// joint groups
-            addOperationCaller(listGroups);
-            addOperationCaller(getGroupIndex);
-            addOperationCaller(getGroupJoints);
-            addOperationCaller(getGroupChains);
-            addOperationCaller(getGroupsChains);
+			addOperationCaller(listGroups);
+			addOperationCaller(getGroupIndex);
+			addOperationCaller(getGroupJoints);
+			addOperationCaller(getGroupChains);
+			addOperationCaller(getGroupsChains);
 			// joints
-            addOperationCaller(listJoints);
-            addOperationCaller(getJointIndex);
-            addOperationCaller(getJointGroup);
-            addOperationCaller(getJointsGroups);
-            addOperationCaller(getJointLowerLimit);
-            addOperationCaller(getJointUpperLimit);
+			addOperationCaller(listJoints);
+			addOperationCaller(getJointIndex);
+			addOperationCaller(getJointGroup);
+			addOperationCaller(getJointsGroups);
+			addOperationCaller(getJointLowerLimit);
+			addOperationCaller(getJointUpperLimit);
 			// contacts
-            addOperationCaller(listContacts);
-            addOperationCaller(getContactPoints);
-            addOperationCaller(addContactPointsToBuffer);
-        }
+			addOperationCaller(listContacts);
+			addOperationCaller(getContactPoints);
+			addOperationCaller(addContactPointsToBuffer);
+		}
 };
 
 } // namespace motion
